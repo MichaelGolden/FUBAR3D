2c7d57e3b883aa99e9c73f04f04c52ae47bb0264
Benjamin P. Kellman <bkellman@ucsd.edu>
2014-12-03 13:12:43 -0800

    fixed more type errors and added the writedlm3D function for writing 3D arrays

--- isingSim.jl
+++ isingSim.jl
@@ -3,28 +3,33 @@ include("MCMC.jl")
 # phase transition at β ≈ 2.5
 
 # run ising model
-function isingSim(S::SparseMatrixCSC{Float64,Int64},β::Float64,maxIter=1e4)
+function isingSim(S::SparseMatrixCSC{Float64,Int64},β::Float64,grid,maxIter=1e4)
     # run ising model given structure (weighted graph)
-    mainMCMC(S,β,maxIter)
+    mainMCMC(S,grid,β,maxIter)
     # return vector of states  Array{Int8,1}
 end
 
 # β repitition & selection
-function repβ(S::SparseMatrixCSC{Float64,Int64},β::FloatRange{Float64}=1:.1:5,rep::Int64=10)
-#  rep>typemax(typeof(rep)) && error("rep must be less than",typemax(typeof(rep)))
-  vβ = Array{Int8,1}
-  for b in β
+function repβ(S::SparseMatrixCSC{Float64,Int64},grid,β::FloatRange{Float64}=1:.1:5,rep::Int64=10)
+  Mβ = zeros(Int,(size(S)[1],rep,length(β)))
+  for b in 1:length(β)
     for r in 1:rep
-#      for i in isingSim(S,b)
-        push!(vβ,isingSim(S,b))
- #     end
+      i = isingSim(S,β[b],grid)
+      Mβ[:,r,b] += i
     end
   end
-  Mβ = reshape(vβ,(size(S)[1],rep,length(β)))
-#  Mβ = sizehint(Array(Int8,1), size(S)[1]*rep*length(β))
-#  Mβ = [i for i=isingSim(S,b),r=1:rep,b=β]
+  Mβ
 end
 
 ### test
-out = repβ(sparse(readdlm("datasets/distance.matrix.thresh15.csv",' ')))
+out = repβ(sparse(readdlm("datasets/distance.matrix.thresh15.csv",' ')),"datasets/rhodopsin.nwk.grid_info",2:.25:3,3)
+
+function writedlm3D(m,title,d3Titles,dlm)
+  if length(d3Titles) != size(m)[3]
+    error("length(d3Titles) != size(m)[3] :: d3Titles is the titles of the 3rd dimension of the array.")
+  end
+  for i in 1:(size(m)[3])
+    writedlm(string(title,".",d3Titles[i],".csv"),m[:,:,i],dlm)
+  end
+end
 
