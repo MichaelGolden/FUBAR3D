d42b5138bd8d0e06bf6a62f37f26c40fd748fe4b
Benjamin P. Kellman <bkellman@ucsd.edu>
2014-12-04 17:56:31 -0800

    Dear Tony, main code to be called from command line. Fixed a type error -Ben

--- FUBAR3D.main.jl
+++ FUBAR3D.main.jl
@@ -2,6 +2,7 @@
 #ex: julia FUBAR3D.main.jl datasets/rhodopsin.nwk.grid_info datasets/distance.matrix.thresh65.csv
 #ex: julia FUBAR3D.main.jl datasets/rhodopsin.nwk.grid_info datasets/distance.matrix.thresh65.csv 20
 
+
 # find conditionals
 gridInfoFile = ARGS[1]
 distanceMatrixFile = ARGS[2]
@@ -13,18 +14,44 @@ end
 
 #RUN MCMC
 include("isingSim.jl")
-try
+ R = readdlm(distanceMatrixFile,' ')
+
+for i in 1:size(R)[1]
+  for j in 1:size(R)[2]
+    if R[i,j] > thresh
+      R[i,j] = 0
+    end
+  end
+end
+S = sparse(R)
+β = 0.0
+isingSim(S,β,gridInfoFile,1e4)
+
+
+
+function broken_shit()
+  try
+        println(1)
   R = readdlm(distanceMatrixFile,' ')
+          println(1)
+
   if size(R)[1]!=size(R)[2]
+        println(1)
     error("R is not square")
+        println(1)
   end
 catch e
+        println(1)
   try
+            println(1)
     R = readdlm(distanceMatrixFile,',')
+        println(1)
     if size(R)[1]!=size(R)[2]
+        println(1)
       error("R is not square")
     end
   catch e
+            println(1)
     try
       R = readdlm(distanceMatrixFile,'\t')
       if size(R)[1]!=size(R)[2]
@@ -35,14 +62,4 @@ catch e
     end
   end
 end
-
-for i in 1:size(R)[1]
-  for j in 1:size(R)[2]
-    if R[i,j] > thresh
-      R[i,j] = 0
-    end
-  end
 end
-S = sparse(R)
-β = 0
-isingSim(S,gridInfoFile,1e4)
