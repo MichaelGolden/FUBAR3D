27d2070f51b26a1297091e909643dbdd53dc1ec9
Arjun <arjunc12@gmail.com>
2014-12-05 14:12:19 -0800

    added probability color gradient to edges

--- protein_graph.R
+++ protein_graph.R
@@ -1,60 +1,73 @@
 library(rgl)
 library(fields)
 library(pdist)
+library(magic)
+library(abind)
+library(geometry)
+#library(combinat)
+library(spam)
+library(grid)
+library(grDevices)
 
 points <- read.table("datasets/hiv1.coords", sep="\t", header=TRUE)
 points <- na.omit(points)
 
 s <- seq(1, ncol(points), by=3)
 # split the 3n columns into n data frames
-# data frame columns contain 
+# data frame columns contain x, y, z coordinates for the peptide
+# rows contain the xyz coordinate for each site
 frames <- lapply(s, function(x) points[,c(x, x + 1, x + 2)])
 
-lapply(frames, function(f) 
-  {plot3d(f[,1], f[,2], f[,3], col=sample(colors(), 1), add=TRUE)})
+# 3-d plot the coordinates at each site for each peptide
+plot_peptide <- function(p)
+{
+  plot3d(p[,1], p[,2], p[,3], col=sample(colors(), 1), add=TRUE)
+  ts.surf <- t(convhulln(p))
+  #rgl.triangles(p[ts.surf,1],p[ts.surf,2],p[ts.surf,3],
+   #            col="blue",alpha=0.01)
+}
+lapply(frames, plot_peptide)
 
-max_dist = 20
+max_dist = 10
 
+# function to draw segments between all pairs of close points
 connect_groups <- function(g)
 {
   g1 = g[[1]]
   g2 = g[[2]]
   
-  #print(g1)
-  #print(g2)
-  
+  colors = replicate(nrow(g1), runif(nrow(g1)))
   close = fields.rdist.near(g1, g2, delta=max_dist, 
                             max.points=nrow(g1)^2)
   close_inds = close$ind
-  #print(close_inds)
   close_inds <- close_inds[close_inds[,1] != close_inds[,2],]
   
   close1 = close_inds[,1]
   close2 = close_inds[,2]
   
-  #print(close1)
-  #print(close2)
-  
   g1 = g1[close1,]
   g2 = g2[close2,]
   
-  #print(g1)
-  #print(g1)
-  
-  #print(g1[,3])
-  #print(g2[,3])
-  
-  colors = replicate(nrow(g1), runif(nrow(g1)))
   colors = colors[close1, close2]
   colors = diag(colors)
+  ncolors = 10000
+  ramp <- heat.colors(ncolors)
+  bins <- seq(0, 1, 1/ncolors)
+  colors <- .bincode(colors, bins, include.lowest=TRUE)
+  colors <- ramp[colors]
   
-  #print(length(colors))
-  #print(nrow(g1))
   
   segments3d(c(g1[,1], g2[,1]), c(g1[,2], g2[,2]), c(g1[,3], g2[,3]), 
-             add=TRUE, col=10*colors)
-  return(1)
+             add=TRUE, col=colors)
 }
 
-combs <- combn(frames, 2, FUN=connect_groups)
-
+# iterate through every combination of two peptides
+# for each combination apply the function to connect close sites
+# between those two peptide pairs
+for (frame1 in frames)
+{
+  for (frame2 in frames)
+  {
+    connect_groups(list(frame1, frame2))
+  }
+}
